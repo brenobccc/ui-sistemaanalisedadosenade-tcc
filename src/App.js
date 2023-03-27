30ecea59c14188974ea2fe7dfb5bb87d27b0bac9
brenobccc <brenobritobc@gmail.com>
2023-03-27 19:09:18 -0300

    iniciando projeto front

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
-
+import TelaPrincipal from "./components/TelaPrincipal/TelaPrincipal";
+import './app.css'
 function App() {
   return (
+    // <div className='app'>teste</div>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <TelaPrincipal />
     </div>
   );
 }
---
+++ src/components/TelaPrincipal/GraficoComparacaoNotas.jsx
@@ -0,0 +1,91 @@
+import React, { useEffect } from "react";
+import Chart from 'chart.js/auto';
+import './GraficosComparaNotas.css';
+
+export default function GraficoComparacaoNotas() {
+
+    const ies1 = {
+        nome: "Universidade A",
+        notas: [6.5, 7.2, 8.0, 6.8]
+    };
+    const ies2 = {
+        nome: "Universidade B",
+        notas: [5.8, 6.1, 7.0, 6.9]
+    };
+
+    // Labels dos anos
+    const labels = ["2010", "2014", "2017", "2021"]
+    //const labels = ["2009", "2011", "2013", "2015", "2017", "2019", "2021", "2023"];
+
+    // Dados do gráfico
+    const data = {
+        labels: labels,
+        datasets: [
+            {
+                label: ies1.nome,
+                data: ies1.notas,
+                borderColor: "red",
+                backgroundColor: "rgba(255, 0, 0, 0.1)",
+                fill: true,
+                tension: 0.3
+            },
+            {
+                label: ies2.nome,
+                data: ies2.notas,
+                borderColor: "blue",
+                backgroundColor: "rgba(0, 0, 255, 0.5)",
+                fill: true,
+                tension: 0.3
+            }
+        ]
+    };
+
+    // Configurações do gráfico
+    const options = {
+        
+        responsive: false,
+        //responsive: true,
+        plugins: {
+            title: {
+                display: true,
+                text: "Notas das IES no Enade"
+            },
+            legend: {
+                display: true,
+                position: 'right'
+            }
+        },
+        scales: {
+            x: {
+                title: {
+                    display: true,
+                    text: "Ano"
+                }
+            },
+            y: {
+                title: {
+                    display: true,
+                    text: "Nota"
+                },
+                ticks: {
+                    beginAtZero: true
+                }
+            }
+        }
+    };
+
+    useEffect(() => {
+        const ctx = document.getElementById("myChart").getContext("2d");
+        const myChart = new Chart(ctx, {
+            type: "line",
+            data: data,
+            options: options
+        });
+    }, []);
+
+    return (
+        <div id="chartjsexample">
+            <canvas id="myChart"></canvas>
+        </div>
+    );
+}
