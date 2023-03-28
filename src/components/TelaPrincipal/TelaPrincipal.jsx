1cd8cd7c6fe8a131c81bb245fcfadf1ff78d0ac7
brenobccc <brenobritobc@gmail.com>
2023-03-28 15:36:50 -0300

    subindo modificacoes

--- src/components/TelaPrincipal/TelaPrincipal.jsx
+++ src/components/TelaPrincipal/TelaPrincipal.jsx
@@ -16,6 +16,15 @@ export default function TelaPrincipal() {
                 </div>
                 <div id="pc-content">
                     <GraficoComparacaoNotas></GraficoComparacaoNotas>
+                    <div id="grafico2">
+                        
+                    </div>
+                    <div id="grafico3">
+                        
+                    </div>
+                    <div id="grafico4">
+                        
+                    </div>
                     {/* <p>
                         There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.
                     </p>
